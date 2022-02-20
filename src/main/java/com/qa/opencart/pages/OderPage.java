7fd589b1f88580de922772ae5c3d430367f82206
DianaDennis <kovas06@yahoo.com>
2022-02-20 10:28:59 -0500

    OderPage modified

--- src/main/java/com/qa/opencart/pages/OderPage.java
+++ src/main/java/com/qa/opencart/pages/OderPage.java
@@ -1,8 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OderPage {
+	
+	By oder = By.id("order");
 
 	public void oder() {
-		System.out.println("oderPage");
+		System.out.println("oder id : 12345");
 	}
 }
